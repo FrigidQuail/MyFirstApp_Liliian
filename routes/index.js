b5a09d2494c49e1ce39ed0459adc71f3bf1331b9
Jen <jenhansonify@gmail.com>
2024-03-21 16:46:19 +0100

    routes update

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,28 @@ router.get("/", function (req, res, next) {
   res.send({ title: "Express" });
 });
 
+router.post('/api/tick-item', (req, res) => {
+  // Extract data from the request body
+  const { itemId, isChecked } = req.body;
+  
+  res.status(200).json({ message: 'Item ticked successfully' });
+});
+
 module.exports = router;
 
-app.get('/api/data', async (req, res) => {
-  try {
-      // Fetch data from the database
-      //but where/how to indicate the right db from model folder?
-      const data = await fetchDataFromDatabase();
+// module.exports = router;
 
-      // Send response back to client with the fetched data
-      res.json(data);
-  } catch (error) {
-      console.error('Error fetching data from database:', error);
-      res.status(500).json({ error: 'Internal server error' });
-  }
-});
+// app.get('/api/data', async (req, res) => {
+//   try {
+//       // Fetch data from the database
+//       //but where/how to indicate the right db from model folder?
+//       const data = await fetchDataFromDatabase();
+
+//       // Send response back to client with the fetched data
+//       res.json(data);
+//   } catch (error) {
+//       console.error('Error fetching data from database:', error);
+//       res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
 
